73bf011164420b8848620908f40777a0113fbb40
TorientalVortX <rosedbu@gmail.com>
2022-09-11 18:36:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/utils.ts
@@ -0,0 +1,28 @@
+import {setTimeout as sleep} from 'node:timers/promises'
+
+import type {VercelResponse} from '@vercel/node'
+import type {AxiosError} from 'axios'
+
+const storage = new Map<string, [number, number]>()
+
+function unixMs(): number {
+	return Date.now()
+}
+
+async function dynamicSleep(name: string): Promise<void> {
+	if (!storage.has(name) || unixMs() - storage.get(name)![0] > 2500) {
+		storage.set(name, [unixMs(), 0])
+	}
+
+	await sleep(400 * storage.get(name)![1]++) // 2000 / 5 = 400ms
+}
+
+function handleError(error: AxiosError, response: VercelResponse): void {
+	if (error.response) {
+		response.status(error.response.status).send(error.response.data)
+	} else {
+		response.status(400).send({error: error.message})
+	}
+}
+
+export {dynamicSleep, handleError}
